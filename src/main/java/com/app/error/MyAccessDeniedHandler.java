32419adc3d060c2faba480de15e4ac24414600f2
arif hosain <arif7hosain@gmail.com>
2017-07-08 11:45:26 +0600

    updated 3

--- src/main/java/com/app/error/MyAccessDeniedHandler.java
+++ src/main/java/com/app/error/MyAccessDeniedHandler.java
@@ -1,4 +1,4 @@
-package com.mkyong.error;
+package com.app.error;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/com/app/model/AbstractModel.java
+++ src/main/java/com/app/model/AbstractModel.java
@@ -1,4 +1,4 @@
-package model;
+package com.app.model;
 
 import java.time.ZonedDateTime;
 
